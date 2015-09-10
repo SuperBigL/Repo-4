3a3f47bb21db17838a40adb8a64211c35038ee10
SuperBigL <master_biglee@live.ca>
2015-09-10 17:22:31 -0600

    Initial Setup

---
+++ Repo-Web/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Repo_Web.Startup))]
+namespace Repo_Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
